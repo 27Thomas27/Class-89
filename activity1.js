9f20fc15bfa57b4c24660720454f83998068d4f0
27Thomas27 <82878370+27Thomas27@users.noreply.github.com>
2022-04-05 14:39:38 +0530

    Add files via upload

---
+++ activity1.js
@@ -0,0 +1,13 @@
+function updateScore()
+{
+    score = score + 1;
+    document.getElementById("score").innerHTML = "score: " + score
+}
+function SaveScore()
+{
+    localStorage.setItem("score",score);
+}
+function NextPage()
+{
+    window.location = "activity_2.html";
+}
